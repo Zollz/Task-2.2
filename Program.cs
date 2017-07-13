d553657c88afd74e3bde83cd061754827c5d6828
Zollz <eddie_sn@Hotmail.com>
2017-07-13 16:39:05 +1000

    update#1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ public class Program
 {
     public static void Main()
     {
+        Account account = new Account("Eddies Account", 200000);
 
+        account.Print()'
+        account.Deposit(100);
+        account.Print();
     }
 }
