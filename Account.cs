d3fd82437225fcf42be7bfcb5771eb41d056070f
Zollz <eddie_sn@Hotmail.com>
2017-07-14 23:28:07 +1000

    update#4

--- Account.cs
+++ Account.cs
@@ -7,32 +7,31 @@ public class Account
     //Constructor
     public Account(string name, decimal startingBalance)
     {
-    _name = name;
-    _balance = startingBalance;
-    Console.WriteLine("Your name is " + _name + " your account balance is $" + _balance);
-    
+        _name = name;
+        _balance = startingBalance;
+        Console.WriteLine("Your name is " + _name + " your account balance is $" + _balance);
+
     }
 
     public void Deposit(decimal amountToAdd)
     {
-    _balance = _balance + amountToAdd;
+        _balance += amountToAdd;
     }
 
     public void Withdraw(decimal amountToSubtract)
     {
-    _balance = _balance - amountToSubtract;
+        _balance -= amountToSubtract;
     }
 
     public void Print()
     {
-        Console.WriteLine("Your balance is now $" + _balance);
+        Console.WriteLine(_name + ", your balance is now $" + _balance);
     }
 
     public string Name
     {
-    get { return _name; }
+        get { return _name; }
     }
-
 }
 
 
--- Program.cs
+++ Program.cs
@@ -6,16 +6,34 @@ public class Program
 
     public static void Main()
     {
-        Account account = new Account("Eddie", 200000);
-        Account account2 = new Account("Jeff", 2);
-       
+        Account account = new Account("Eddie", 200);
+        Account account2 = new Account("Jeff", 200);
+
+        account2.Deposit(50);
+        Console.WriteLine("Jeff, you deposited $50");
+        account2.Print();
+
+        account.Withdraw(50);
+        Console.WriteLine("Eddie, you withdrew $50");
         account.Print();
 
-  
- 
-     
+        account2.Withdraw(210);
+        Console.WriteLine("Jeff, you withdrew $210");
+        account2.Print();
+
+        account.Withdraw(63);
+        Console.WriteLine("Eddie, you withdrew $63");
+        account.Print();
+
+
+
+
+
+
+
+
     }
- 
-    
+
+
 
 }
